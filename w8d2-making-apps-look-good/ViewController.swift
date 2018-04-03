b314edd093165ad33474cb0636cc2bec865c92de
Roland Tecson <roland_tecson@hotmail.com>
2018-04-03 08:58:27 -0700

    Initial Commit

---
+++ w8d2-making-apps-look-good/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  w8d2-making-apps-look-good
+//
+//  Created by Roland on 2018-04-03.
+//  Copyright © 2018 MoozX Internet Ventures. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
